1e491616073a339f6329b2716bb89bc47cb442c9
Giovanna Linda <eugiovannasouza@gmail.com>
2022-06-10 10:17:39 -0300

    style: config styled-components

---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+export const theme = {
+  font: {
+    family: 'Inter',
+    weigths: {
+      regular: 400,
+      medium: 500,
+      semiBold: 600,
+      bold: 700,
+    },
+  },
+  colors: {
+    white: '#FFFFFF',
+    gray100: '#DFE3EB',
+    gray500: '#A5A5A5',
+    primaryDark: '#242424',
+    secondaryPink: '#ED1CA6',
+    secondaryPurple: '#7D00FF',
+  },
+}
