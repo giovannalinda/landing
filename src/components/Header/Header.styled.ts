d83a527c9e11697cf0ce017a090108e69cfeeea2
Giovanna Linda <eugiovannasouza@gmail.com>
2022-06-14 10:08:08 -0300

    feat: add link navigation in header component

--- src/components/Header/Header.styled.ts
+++ src/components/Header/Header.styled.ts
@@ -7,3 +7,29 @@ export const Header = styled.header`
   margin: 0 auto;
   padding: ${theme.space[6]} 0;
 `
+
+export const Navigation = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  width: 100%;
+
+  ul {
+    display: flex;
+  }
+
+  li {
+    font-weight: ${theme.font.weigths.bold};
+    letter-spacing: ${theme.font.letterSpacing.md};
+    font-size: 1.4rem;
+    z-index: 1;
+
+    a {
+      color: ${theme.colors.white};
+    }
+
+    & + li {
+      margin-left: ${theme.space[7]};
+    }
+  }
+`
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,33 @@
-import { Navigation } from './Navigation'
-
+import Link from 'next/link'
 import * as S from './Header.styled'
 
 export function Header() {
   return (
     <S.Header>
-      <Navigation />
+      <S.Navigation>
+        <ul>
+          <li>
+            <Link href='#about' scroll>
+              ABOUT
+            </Link>
+          </li>
+          <li>
+            <Link href='#services' scroll>
+              SERVICES
+            </Link>
+          </li>
+          <li>
+            <Link href='#works' scroll>
+              WORKS
+            </Link>
+          </li>
+          <li>
+            <Link href='#contact' scroll>
+              CONTACT
+            </Link>
+          </li>
+        </ul>
+      </S.Navigation>
     </S.Header>
   )
 }
