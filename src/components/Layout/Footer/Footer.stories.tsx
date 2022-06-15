09216e83a89b61b7464cebd77c5870e91ae87619
Giovanna Linda <eugiovannasouza@gmail.com>
2022-06-15 18:35:28 -0300

    docs: add footer in storybook

---
+++ src/components/Layout/Footer/Footer.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, Story } from '@storybook/react'
+
+import { Footer } from './Footer'
+
+export default {
+  title: 'Components/Layout/Footer',
+  component: Footer,
+} as Meta
+
+export const Default: Story = () => <Footer />
